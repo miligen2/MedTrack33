db9820be0078632f6172d70cef02e2d28ba2404b
Miligen <angelo0204@hotmail.fr>
2023-09-29 12:20:01 +0200

    first

--- MedTrack/ContentView.swift
+++ MedTrack/ContentView.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView {
+            //page d'accueil ou on retrouve les medicaments à prendre
+            Index()
+                .tabItem {
+                Label("À prendre", systemImage: "checklist.checked")
+            }
+            // Permets de pouvoir parametre un reveil mettre les traitements dans index
+                Traitement()
+                .tabItem {
+                    Label("Traitement", systemImage: "pill.fill")
+                }
+            // rappel sur un calendrier sur la prise de medicament
+                Calendar()
+
+                .tabItem {
+                    Label("Calendrier", systemImage: "calendar")
+                }
+            // Chat intégerer pour pouvoir parler avec un médecin
+//                Chat()
+//                .tabItem{
+//                    Label("Chat", systemImage: "ellipsis.message")
+//                }
+      }
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
