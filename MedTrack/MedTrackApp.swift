69482ed62038982b090b5946b0a0e0949b7df5b7
Miligen <angelo0204@hotmail.fr>
2023-09-29 12:04:51 +0200

    Initial Commit

---
+++ MedTrack/MedTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedTrackApp.swift
+//  MedTrack
+//
+//  Created by Angelo Macaire on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MedTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
