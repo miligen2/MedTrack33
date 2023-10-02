1ec72a2e865ba5ced09a7d07ee1ad4605ddb72d0
Miligen <angelo0204@hotmail.fr>
2023-10-02 18:35:36 +0200

    rendu final 2.0

--- MedTrack/Modal/ModalMedicament.swift
+++ MedTrack/Modal/ModalMedicament.swift
@@ -7,14 +7,12 @@ struct Medicament:Identifiable,Hashable {
 
 struct PillData {
     let id: Int
+    let nom: String
     let nombreDeFoisParJour: Int
     let heureDePrise1: String
     let heureDePrise2: String
     let heureDePrise3: String
     let nombreDeComprimes: Int
-}
-struct PillDataInBox {
-    let id: Int
     let nombreDeComprimesDansBoite: Int
     let rappelComprimes: Int
 }
